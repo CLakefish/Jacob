eb38aa84ed76d5d22a5ac319041069dad46b2245
TaloozaGames <80441141+TaloozaGames@users.noreply.github.com>
2023-09-14 18:27:39 -0700

    added enemy spawner

---
+++ WANICYear2Project1/Assets/EnemySpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject EnemyPrefab;
+
+    public int SpawnDistance;
+    public int MaxSpawnDistance;
+
+    public int EnemiesPerSpawn;
+
+    public float SpawnRate;
+    public int MaxSpawnRate;
+
+    public float DifficultyRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for every nth second, spawn an enemy at a random point inbetween min spawn distance and max spawn distance, reset spawn rate;
+        if(SpawnRate < 0)
+        {
+            for(int i = 0; i < EnemiesPerSpawn; i++)
+            {
+                SpawnEnemy();
+            }
+
+            SpawnRate = MaxSpawnRate;
+        }
+        else
+        {
+            SpawnRate = SpawnRate - 1 * Time.deltaTime - DifficultyRate;
+        }
+
+
+    }
+
+    private void IncreaseDifficulty()
+    {
+        if (DifficultyRate > 4) //makes certain that no matter what the gmae will only take away atleast 4 seconds from the spawn rate. set ACTUAL spawn rate to be greater than 8.
+        {
+            DifficultyRate = DifficultyRate += 0.5f;
+        }
+        EnemiesPerSpawn++;
+    }
+    private void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, new Vector3(Player.transform.position.x - Random.Range(SpawnDistance, MaxSpawnDistance), 0, 0), Quaternion.identity);
+        Instantiate(EnemyPrefab, new Vector3(Player.transform.position.x + Random.Range(SpawnDistance, MaxSpawnDistance), 0, 0), Quaternion.identity);
+
+        Debug.Log("enemySpawned!");
+    }
+}
